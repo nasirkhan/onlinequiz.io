f25f955282f590a816b41cfae951dea16293950b
Nasir Khan <nasir8891@gmail.com>
2020-11-10 12:01:48 +0600

    url update

--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -1,15 +1,15 @@
 <nav class="hidden lg:flex items-center justify-end text-lg">
-    <a title="{{ $page->siteName }} Blog" href="/blog"
+    <a title="{{ $page->siteName }} Blog" href="{{ $page->asset('/blog') }}"
         class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/blog') ? 'active text-blue-600' : '' }}">
         Blog
     </a>
 
-    <a title="{{ $page->siteName }} About" href="/about"
+    <a title="{{ $page->siteName }} About" href="{{ $page->asset('/about') }}"
         class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/about') ? 'active text-blue-600' : '' }}">
         About
     </a>
 
-    <a title="{{ $page->siteName }} Contact" href="/contact"
+    <a title="{{ $page->siteName }} Contact" href="{{ $page->asset('/contact') }}"
         class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/contact') ? 'active text-blue-600' : '' }}">
         Contact
     </a>
